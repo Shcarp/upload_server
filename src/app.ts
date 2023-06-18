757c0fc311b665daa04dd10a90ea0fb6089fe0e2
sunhui1 <sunuih@qq.com>
2023-06-18 22:49:46 +0800

    上传bug 修复

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,42 @@
-import Koa from 'koa';
-import Router from 'koa-router';
-import qinue from './upload';
-import koaBody from 'koa-body';
-import path from 'path';
-import fs from 'fs';
+import Koa from "koa";
+import Router from "koa-router";
+import qinue from "./upload";
+import koaBody from "koa-body";
+import path from "path";
+import fs from "fs";
 
 const app = new Koa();
 
-app.use(koaBody({
-    // 支持文件格式
-    multipart: true,
-    // 文件位置
-    formidable: {
-        uploadDir: path.join(__dirname, '../public'),
-        keepExtensions: true,
-    },
-
-}));
+app.use(
+    koaBody({
+        // 支持文件格式
+        multipart: true,
+        // 文件位置
+        formidable: {
+            uploadDir: path.join(__dirname, "../public"),
+            keepExtensions: true,
+        },
+    })
+);
 
 const router = new Router();
 
 // 文件上传
-router.post('/upload', async (ctx) => {
+router.post("/upload", async (ctx) => {
     const file = ctx.request.files?.file;
     if (!file) {
         ctx.body = {
             code: 400,
-            msg: '文件不能为空',
+            msg: "文件不能为空",
         };
         return;
     }
-    // 创建可读流
-    const reader = fs.createReadStream(file.filepath);
-    // 获取文件后缀
-    const ext = file.filename.split('.').pop();
+
     try {
+        // 创建可读流
+        const reader = fs.createReadStream(file.filepath);
+        // 获取文件后缀
+        const ext = file.originalFilename.split('.').pop();
         const res: Record<string, any> = await qinue.uploadFileStream(file.newFilename, reader);
         ctx.body = {
             code: 200,
@@ -43,22 +45,23 @@ router.post('/upload', async (ctx) => {
                 ext
             },
         };
-        // 删除临时文件
-        fs.rm(file.filepath, () => { });
     } catch (error) {
+        console.log(error);
         ctx.body = {
             code: 500,
-            msg: '上传失败',
+            msg: "上传失败",
         };
+    } finally {
+        fs.rm(file.filepath, () => {});
     }
-})
+});
 
-router.get('/download', async (ctx) => {
+router.get("/download", async (ctx) => {
     const { key } = ctx.query;
     if (!key) {
         ctx.body = {
             code: 400,
-            msg: '文件不能为空',
+            msg: "文件不能为空",
         };
         return;
     }
@@ -71,17 +74,17 @@ router.get('/download', async (ctx) => {
     } catch (error) {
         ctx.body = {
             code: 500,
-            msg: '下载失败',
+            msg: "下载失败",
         };
     }
-})
+});
 
-router.delete('/delete', async (ctx) => {
+router.delete("/delete", async (ctx) => {
     const { key } = ctx.query;
     if (!key) {
         ctx.body = {
             code: 400,
-            msg: '文件不能为空',
+            msg: "文件不能为空",
         };
         return;
     }
@@ -94,18 +97,19 @@ router.delete('/delete', async (ctx) => {
     } catch (error) {
         ctx.body = {
             code: 500,
-            msg: '删除失败',
+            msg: "删除失败",
         };
     }
-})
+});
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen({
-    host: '0.0.0.0',
-    port: 8090,
-}, () => {
-    console.log('server is running at http://0.0.0.0:8090');
-})
-
-
+app.listen(
+    {
+        host: "0.0.0.0",
+        port: 8090,
+    },
+    () => {
+        console.log("server is running at http://0.0.0.0:8090");
+    }
+);
